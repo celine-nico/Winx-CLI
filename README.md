71aeebf662d53c67f4532b90e2b2fa19a72a5379
Cel <celinenico22@gmail.com>
2021-03-20 14:07:43 -0400

    Created readme file.

---
+++ README.md
@@ -0,0 +1,19 @@
+# Winx Club Profile
+This application takes a dive into the profiles and characteristics of the main protagonists of the Italian cartoon, WInx CLub. In this application you are able to choose from the main five and explore where their powers come from and the many different abilities they accquire. 
+
+## Motivation
+This show was one of my all time favorite cartoons growing up. It is really interesting the way show creator Ignio Straffi is able to create many unique characters that balance each other's personalities so well. It is interesting to see the different magical abilities that each protagonist controls. It ignites the creativity inside other kids, including myself, to create their own fairy and their own magic. 
+
+## Install 
+For testing this application, clone this repository and run: 
+- `cd Winx_CLI`
+- `bundle install`
+
+To start the application, run:
+- `bin/winx_club`
+
+## Contributors 
+This is a safe, welcoming community that is open for any kind of collaboration. Reports or signs of error are welcome to be reported on GitHub via https://github.com/celine-nico/Winx-CLI . All contributors are expected to follow the [Contributor Covenant](https://www.contributor-covenant.org/). 
+
+## License 
+Gem is available as open source under the [MIT License](http://www.opensource.org/licenses/mit-license) . 
--- lib/cli.rb
+++ lib/cli.rb
@@ -2,7 +2,7 @@ class CLI
 
     def output
         # begins the program witha greeting 
-        puts "We are the Winx!"
+        puts "We are the Winx!".black.on_magenta 
         Scraper.scrape_winx
         list_winx 
         menu 
@@ -13,16 +13,16 @@ class CLI
     def list_winx
         # iterates within the list of characters and outputs each character in a list
         Winx.all.each.with_index(1) do |fairy, i|
-            puts "#{i}. #{fairy.name}"
+            puts "#{i}. #{fairy.name}".light_cyan 
         end 
     end 
 
     def menu
-        puts "Select a Winx!"
+        puts "Select a Winx!".black.on_cyan 
         input = gets.chomp
         # gives user the choice to choose a character 
         if !input.to_i.between?(1,Winx.all.count)
-            puts "That's not a Winx! Again?"
+            puts "That's not a Winx! Again?".cyan 
             list_winx
             menu
             # makes user choose again if the number inputted is not on the list 
@@ -38,10 +38,10 @@ class CLI
         Scraper.scrape_single_winx(winx)
         block = "[A-Z][a-z]+"
         # assigns a variable that will take out any weird spaces inside a list
-        puts "\nHi, I'm #{winx.name}!"
-        puts "\nAnd I'm the #{winx.alias}!"
+        puts "\nHi, I'm #{winx.name}!".yellow
+        puts "\nAnd I'm the #{winx.alias}!".yellow 
 
-        puts "\nMy powers include: \n\n#{winx.power.scan(/#{block}(?:\s+#{block})*/).join("\n")}"
+        puts "\nMy powers include: \n\n#{winx.power.scan(/#{block}(?:\s+#{block})*/).join("\n")}".light_yellow
         # outputs the powers in a list, taking out weird spaces and putting each in a new line 
     end 
     # outputs the charater name, alias and attributes
